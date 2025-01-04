eaafc3b5ec2af5ab615b1c1394381d4a0dd0a0c7
ydj <yaodj@sino-bridge.com>
2025-01-04 20:04:45 +0800

    perf: 优化页面布局

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## 使用vite/express/pngquant-bin搭建的图片压缩web应用
-
+<!--
 ## TODO
 
-* [ ] 支持多文件上传和压缩
+* [ ] 支持多文件上传和压缩 -->
+
+![alt text](banner.png)
--- vite-project/src/components/Upload.vue
+++ vite-project/src/components/Upload.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="container">
-    <div>
-        <!-- :limit="1"
+  <div style="margin-top: 20px">
+    <h2 style="text-align: center">在线图片压缩</h2>
+    <!-- :limit="1"
         :on-exceed="handleExceed" -->
-      <el-upload
-        accept=".png"
-        drag
-        action="/compress"
-        :show-file-list="false"
-        :before-upload="beforeUpload"
-        :on-success="handleSuccess"
-        :on-error="handleError"
-        ref="upload">
-        <el-button
-          size="small"
-          type="primary">
-          点击上传图片
-        </el-button>
-      </el-upload>
-    </div>
+    <el-upload
+      accept=".png"
+      drag
+      style="width: 600px; margin: 0 auto"
+      action="/compress"
+      :show-file-list="false"
+      :before-upload="beforeUpload"
+      :on-success="handleSuccess"
+      :on-error="handleError"
+      ref="upload">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32">
+        <path
+          fill="currentColor"
+          d="M16 4c-4.12 0-7.36 3.13-7.813 7.125a4.9 4.9 0 0 0-3.843 3.22C1.884 15.054 0 17.248 0 20c0 3.324 2.676 6 6 6h20c3.324 0 6-2.676 6-6c0-1.76-.855-3.336-2.094-4.438c-.232-3.514-3.035-6.318-6.562-6.5C22.14 6.133 19.378 4 16 4m0 2c2.762 0 4.97 1.77 5.75 4.28l.22.72H23c2.755 0 5 2.245 5 5v.5l.406.313A4.07 4.07 0 0 1 30 20c0 2.276-1.724 4-4 4H6c-2.276 0-4-1.724-4-4c0-2.02 1.45-3.588 3.28-3.906l.657-.125l.125-.658C6.362 13.964 7.556 13 9 13h1v-1c0-3.37 2.63-6 6-6m0 5.594l-.72.687l-4 4l1.44 1.44L15 15.437V22h2v-6.563l2.28 2.282l1.44-1.44l-4-4z" />
+      </svg>
+      <div style="font-size: 12px; color: gray">拖动/粘贴/点击上传图片</div>
+    </el-upload>
 
     <!-- 显示文件大小 -->
-    <div
-      v-if="originalSize !== null && newSize !== null"
-      class="file-sizes">
-      原文件大小: {{ originalSize }} KB | 压缩后大小: {{ newSize }} KB
-      <br />
-      压缩率为: {{ compressionRate }}%
-      <br />
-      大小减少了: {{ sizeReduced }} KB
-    </div>
-
-    <!-- 显示压缩后的图片 -->
-    <div style="display: flex; justify-content: center; flex-direction: column">
-      <div>
-        <img
-          v-if="imageSrc"
-          :src="imageSrc"
-          alt="压缩结果"
-          style="border-radius: 5px; max-height: 50vh; max-width: 50vw" />
+    <div style="margin: 0 auto; width: 600px;text-align: center;">
+      <div
+        v-if="originalSize !== null && newSize !== null"
+        class="file-sizes">
+        原文件大小: {{ originalSize }} KB | 压缩后大小: {{ newSize }} KB | 压缩率为: {{ compressionRate }}% | 大小减少了: {{ sizeReduced }} KB
       </div>
-      <div>
-        <el-button
-          size="small"
-          v-if="imageSrc"
-          @click="copyImage">
-          复制图片
-        </el-button>
-        <el-button
-          size="small"
-          v-if="imageSrc"
-          @click="downloadImage">
-          下载图片
-        </el-button>
+
+      <!-- 显示压缩后的图片 -->
+        <div>
+          <img
+            v-if="imageSrc"
+            :src="imageSrc"
+            alt="压缩结果"
+            style="border-radius: 5px; max-height: 50vh;" />
+        </div>
+        <div>
+          <el-button
+            v-if="imageSrc"
+            plain
+            type="primary"
+            @click="copyImage">
+            复制图片
+          </el-button>
+          <el-button
+            v-if="imageSrc"
+            plain
+            type="success"
+            @click="downloadImage">
+            下载图片
+          </el-button>
       </div>
     </div>
   </div>
@@ -194,11 +197,6 @@ onUnmounted(() => {
 </script>
 
 <style>
-.container {
-  text-align: center;
-  padding: 20px;
-}
-
 img {
   max-width: 100%;
   margin: 20px 0;
